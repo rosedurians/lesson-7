ef13c2d6997084ec9fdf3e99b02f2057c6f66ebc
rosedurians <44170550+rosedurians@users.noreply.github.com>
2018-12-10 11:29:11 -0500

    Add files via upload

---
+++ Problem 2.py
@@ -0,0 +1,11 @@
+x = input('Pick a number: ')
+x = int(x)
+y = input('Pick another number: ')
+y = int(y)
+
+
+product = x + y
+product = str(product)
+
+print(product)
+
---
+++ Problem 3.py
@@ -0,0 +1,5 @@
+age = input('How old are you? ')
+age = int(age)
+yearsleft = 100 - age
+answer = 2018 + yearsleft
+print(answer)
